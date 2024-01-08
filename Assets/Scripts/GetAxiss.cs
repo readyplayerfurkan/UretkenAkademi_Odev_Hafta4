4366bf74c9e569ca5be69502fce902de11ad395a
Furkan Yılmaz <furkanyilmaz870@gmail.com>
2024-01-08 23:35:43 +0300

    feat: Applied get axis examples

---
+++ Assets/Scripts/GetAxiss.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class GetAxiss : MonoBehaviour
+{
+    private float _verticalInputValue;
+    private float _horizontalInputValue;
+
+    private void Update()
+    {
+        _horizontalInputValue = Input.GetAxis("Horizontal");
+        _verticalInputValue = Input.GetAxis("Vertical");
+
+        switch (_horizontalInputValue)
+        {
+            case > 0:
+                Debug.Log("You are moving to the right.");
+                break;
+            case 0:
+                Debug.Log("You are standing.");
+                break;
+            default:
+                Debug.Log("You are moving to the left.");
+                break;
+        }
+        
+        switch (_verticalInputValue)
+        {
+            case > 0:
+                Debug.Log("You are moving to the sky.");
+                break;
+            case 0:
+                Debug.Log("You are standing.");
+                break;
+            default:
+                Debug.Log("You are moving to the ground.");
+                break;
+        }
+    }
+}
